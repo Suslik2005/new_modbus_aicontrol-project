7c9360f04056174325bc2d586dba8a37da2e0d54
Ruslan_Osipov <95231140+Suslik2005@users.noreply.github.com>
2024-11-20 16:42:04 +0300

    Add files via upload

--- main_window.py
+++ main_window.py
@@ -16,14 +16,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
-    QMainWindow, QPushButton, QSizePolicy, QVBoxLayout,
-    QWidget)
+    QLineEdit, QMainWindow, QPushButton, QSizePolicy,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(357, 251)
+        MainWindow.resize(491, 227)
         MainWindow.setStyleSheet(u"background-color:  rgba(160,160,160, 130);")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
@@ -35,6 +35,16 @@ class Ui_MainWindow(object):
         self.pushButton.setObjectName(u"pushButton")
 
         self.verticalLayout.addWidget(self.pushButton)
+
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.verticalLayout.addWidget(self.lineEdit)
+
+        self.comboBox = QComboBox(self.centralwidget)
+        self.comboBox.setObjectName(u"comboBox")
+
+        self.verticalLayout.addWidget(self.comboBox)
 
         self.comboBox_2 = QComboBox(self.centralwidget)
         self.comboBox_2.addItem("")
@@ -75,15 +85,16 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"main_window", None))
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u043f\u043e\u0434\u043a\u043b\u044e\u0447\u0438\u0442\u044c\u0441\u044f", None))
-        self.pushButton.setStyleSheet(
-            'QPushButton {width: 230px; height: 50 px; color: white;}')
-        self.comboBox_2.setItemText(0, QCoreApplication.translate("MainWindow", u"\u043a\u043e\u043b\u0438\u0431\u0440\u043e\u043b\u0432\u043a\u0430 \u043e\u0442\u043a\u043b\u044e\u0447\u0435\u043d\u0430", None))
+        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"ip", None))
+        self.comboBox.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u043f\u043e\u0441\u043b\u0435\u0434\u043d\u0438\u0435  \u0438\u0441\u043f ip", None))
+        self.comboBox_2.setItemText(0, QCoreApplication.translate("MainWindow", u"\u043a\u0430\u043b\u0438\u0431\u0440\u043e\u0432\u043a\u0430 \u043e\u0442\u043a\u043b\u044e\u0447\u0435\u043d\u0430", None))
         self.comboBox_2.setItemText(1, QCoreApplication.translate("MainWindow", u"\u043d\u0430\u0447\u0430\u0442\u044c \u043a\u0430\u043b\u0438\u0431\u0440\u043e\u0432\u043a\u0443", None))
         self.comboBox_2.setItemText(2, QCoreApplication.translate("MainWindow", u"\u0441\u043e\u0445\u0440\u0430\u043d\u0438\u0442\u044c \u043a\u0430\u043b\u0438\u0431\u0440\u043e\u0432\u043a\u0443", None))
         self.comboBox_2.setItemText(3, QCoreApplication.translate("MainWindow", u"\u0432\u0435\u0440\u043d\u0443\u0442\u044c \u0437\u0430\u0432\u043e\u0434\u0441\u043a\u0443\u044e \u043a\u0430\u043b\u0438\u0431\u0440\u043e\u0432\u043a\u0443", None))
         self.comboBox_2.setItemText(4, QCoreApplication.translate("MainWindow", u"\u0437\u0430\u0434\u0430\u0442\u044c \u0442\u0435\u0440\u043c\u043e\u043a\u043e\u043c\u043f\u0435\u043d\u0441\u0430\u0446\u0438\u044e(-)", None))
         self.comboBox_2.setItemText(5, QCoreApplication.translate("MainWindow", u"\u0437\u0430\u0434\u0430\u0442\u044c \u0442\u0435\u0440\u043c\u043e\u043a\u043e\u043c\u043f\u0435\u043d\u0441\u0430\u0446\u0438\u044e(+)", None))
 
+        self.comboBox_2.setPlaceholderText(QCoreApplication.translate("MainWindow", u"\u0441\u0442\u0430\u0434\u0438\u044f \u043a\u0430\u043b\u0438\u0431\u0440\u043e\u0432\u043a\u0438", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u043e\u043a", None))
--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import sys
 import threading
 import time
-from pickle import FALSE
+import os
 
 import PySide6
 from PySide6 import QtWidgets, QtGui
@@ -10,13 +10,21 @@ from pymodbus.client import ModbusTcpClient
 
 from main_window import Ui_MainWindow
 
-
+DB_NAME = "ip.db"
 class Modbus(QMainWindow):
     def __init__(self):
         super(Modbus, self).__init__()
+        #создание txt файла с последними ip
+        self.FILE_NAME = "ips.txt"
+        if not os.path.exists(self.FILE_NAME):
+            with open(self.FILE_NAME, "w") as file:
+                pass  # Просто создаём пустой файл
+        #до сюда
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.client = None
+        self.control_result = 0
+        self.ui.pushButton_2.setEnabled(False)
         # словарь для отправки значений
         self.main_dict = {"калибролвка отключена": 1, 'начать калибровку': 2, "сохранить калибровку": 3,
                           "вернуть заводскую калибровку": 4, "задать термокомпенсацию(-)": 5,
@@ -24,23 +32,39 @@ class Modbus(QMainWindow):
         self.stadiya_colibrovki = {1: "калибровка отключена", 2: "подайте 4мА (1 В)", 3: "подайте 20мА (5 В)",
                                    4: "сохранить калибровку?", 5: "подайте 8мА (2 В)", 6: "подайте 12мА (3 В)",
                                    7: "подайте 16мА (4 В)", 8: "ожидание стабилизации сигнала", 9: "калиброка"}
+        self.refresh_ips()
         # кнопка подключения
         self.ui.pushButton.clicked.connect(self.connection)
         # кнопка для передачи значения в "действия при калибровке"
         self.ui.pushButton_2.clicked.connect(self.ask)
 
     # при отправке значения запускается поток
+
+    def write_ip_to_file(self, ip):
+        with open(self.FILE_NAME, "a") as file:
+            file.write(f"{ip}\n")
+        print(f"IP '{ip}' записан в файл.")
     def potok(self):
         monitoring_thread = threading.Thread(target=self.surveillance())
         monitoring_thread.daemon = True  # Закрыть поток при завершении программы
         monitoring_thread.start()
 
     def surveillance(self):
+        label = self.ui.label
         while True:
             time.sleep(10)
             result = self.client.read_holding_registers(10031, 1)
             print(result)
+            if self.control_result != self.stadiya_colibrovki[int(result)]:
+                self.control_result = self.stadiya_colibrovki[int(result)]
+                label.clear()  # Очищаем текст в QLabel
+                label.setText(self.control_result)
 
+    # Функция для чтения IP из файла в обратном порядке
+    def read_ips_from_file(self):
+        with open(self.FILE_NAME, "r") as file:
+            lines = file.readlines()
+        return [line.strip() for line in reversed(lines)]  # Удаляем лишние символы
     # функция для отправки значения в 10030 индекс
     def ask(self):
         vaalue = self.ui.comboBox_2.currentText()
@@ -50,18 +74,35 @@ class Modbus(QMainWindow):
         self.client.write_register(10030, self.main_dict[vaalue])
         self.potok()
 
-    #
+    def refresh_ips(self):
+        self.ui.comboBox.clear()
+        ips = self.read_ips_from_file()
+        self.ui.comboBox.addItems(ips)
+        print("IP-адреса обновлены в comboBox.")
     # подключение к ModbusTcp
     def connection(self):
-        self.client = ModbusTcpClient('192.168.55.79')
+        text = self.ui.lineEdit.text()
+        if text == "":
+            text = self.ui.comboBox.currentText()
+        self.client = ModbusTcpClient(text)
         button = self.ui.pushButton
         # значение при подключении (True/False)
         a = self.client.connect()
         # есть подключение - кнопка зеленая, нет подключения - кнопка красная
         if a:
             button.setEnabled(False)
-            button.setStyleSheet(
-                'QPushButton {background-color: rgba(0,255,0,30);width: 230px; height: 50 px; color: white;}')
+            button.setStyleSheet("""
+                QPushButton:disabled {
+                    background-color: rgba(0,255,0,30);
+                    width: 230px; 
+                    height: 50 px;
+                    color: white;
+                }
+            """)
+            self.ui.pushButton_2.setEnabled(True)
+            self.write_ip_to_file(text)
+            self.refresh_ips()
+
         else:
             button.setStyleSheet(
                 'QPushButton {background-color: rgba(255,0,0,30);width: 230px; height: 50 px; color: white;}')
